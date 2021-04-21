2063b6fcf789083eb8d008041d19399102350d79
Thiernope <ntirandth@gmail.com>
2021-04-21 21:53:07 +0200

    ft(process section): add process section
       - implement process design
    
    [deliver

---
+++ betahills-app/src/components/Process.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Process() {
+    return (
+        <div className = "bg-white pl-28 py-16">
+           <div className="w-4/6 p-8">
+               <h1 className ="text-6xl text-justify">Seamless integration.<br/>Thorough testing<br/>Better result</h1>
+           </div>
+
+           <div className = "flex justify-end">
+               <p className="w-1/2 p-8 text-justify"> 
+               Our software testers are led by project managers and lead developers from the US, Canada and Europe with more than 10 years of experience in QA and software testing. This guarantees clear communication and a team with an in-depth understanding of your business and testing needs. After an initial assessment, we tailor our workflow to seamlessly integrate into your current processes. We can use your preferred tools and adapt our services based on your communication models, timeline, and more
+               </p>
+           </div>
+        </div>
+    )
+}
+
+export default Process
